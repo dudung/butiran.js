dabe0ecd5ba68f7b4849d1a69b42f5139b2cc5b9
unknown <dudung@gmail.com>
2019-05-28 08:46:07 +0700

    update app/cppcmf

--- app/cppcmf/cppcmf.js
+++ app/cppcmf/cppcmf.js
@@ -7,11 +7,13 @@
 	
 	20190528
 	0446 Start this app.
+	0754 Continue at campus.
 */
 
 // Define global variables
 var taIn, taOut, caOut;
-var btClear, btLoad, btRead, btStart, btInfo;
+var btLoad, btRead, btStart, btInfo;
+var params;
 
 
 // Execute main function
@@ -26,12 +28,79 @@ function main() {
 
 // Create visual elements
 function createVisualElements() {
+	// Create textarea for input
 	taIn = document.createElement("textarea");
-	taIn.style.overflowY = "scroll";
-	with taIn {
-		
+	with(taIn.style) {
+		overflowY = "scroll";
+		width = "194px";
+		height = "200px";
 	}
 	
+	// Create textarea for output
+	taOut = document.createElement("textarea");
+	with(taOut.style) {
+		overflowY = "scroll";
+		width = "194px";
+		height = "200px";
+	}
 	
-	document.body.append(taIn);
+	// Create button for loading default parameters
+	btLoad = document.createElement("button");
+	with(btLoad) {
+		innerHTML = "Load";
+		style.width = "50px";
+		addEventListener("click", buttonClick);
+	}
+
+	// Create button for reading shown parameters
+	btRead = document.createElement("button");
+	with(btRead) {
+		innerHTML = "Read";
+		style.width = "50px";
+		addEventListener("click", buttonClick);
+	}
+	
+	// Create button for starting simulation
+	btStart = document.createElement("button");
+	with(btStart) {
+		innerHTML = "Start";
+		style.width = "50px";
+		addEventListener("click", buttonClick);
+	}
+	
+	// Create button for giving information
+	btInfo = document.createElement("button");
+	with(btInfo) {
+		innerHTML = "Info";
+		style.width = "50px";
+		addEventListener("click", buttonClick);
+	}
+	
+	// Create canvas for output
+	
+	
+	// Create div for left part
+	var dvLeft = document.createElement("div");
+	with(dvLeft.style) {
+		width = "200px";
+		height = "442px";
+		border = "1px solid #eee";
+		background = "#eee";
+	}
+	
+	// Append element in structured order
+	document.body.append(dvLeft);
+		dvLeft.append(taIn);
+		dvLeft.append(taOut);
+		dvLeft.append(btLoad);
+		dvLeft.append(btRead);
+		dvLeft.append(btStart);
+		dvLeft.append(btInfo);
+}
+
+
+// Handle event of button click
+function buttonClick() {
+	var t = event.target;
+	console.log(t.innerHTML);
 }
