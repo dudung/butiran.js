e066e5b10661233f41de4bad0f319a10dd3edad6
dudung <dudung@gmail.com>
2019-08-08 17:31:52 +0700

    fin abmdif

--- app/abmdif/abmdif.js
+++ app/abmdif/abmdif.js
@@ -15,19 +15,24 @@
 	1113 Generating three types of particles randomly ok.
 	1118 Remove draw() frin cppcmf.js (template).
 	1137 Agents can move, but only blue.
+	1608 Add second canvas for simple chart.
+	1610 Try to implement series and chart from vratcabm.
+	1619 Test drawSeriesOnCanvas().
+	1635 All seems ok.
 */
 
 // Define global variables
 var params;
-var taIn, taOut, caOut;
+var taIn, taOut, caOut, caOut2;
 var btLoad, btRead, btStart, btInfo;
 var tbeg, tend, dt, t, Tdata, Tproc, proc, iter, Niter;
 var digit;
 var XMIN, XMAX, YMIN, YMAX;
 var Numx, Numy, Btyp, Ntyp;
-var Nums, N, Cofs;
+var Nums, N, Cofs, NumsL;
 var W, A;
-var colors;
+var colors, scen;
+var series;
 
 // Execute main function
 main();
@@ -45,24 +50,27 @@ function initParams() {
 	var p = "";
 	p += "# Iteration\n";
 	p += "TBEG 0\n";
-	p += "TEND 100\n";
+	p += "TEND 1000\n";
 	p += "TSTP 1\n";
 	p += "TDAT 1\n";
-	p += "TPRC 1\n";
+	p += "TPRC 20\n";
 	p += "\n";
 	p += "# Environment\n";
-	p += "NUMX 100\n";
-	p += "NUMY 100\n";
+	p += "NUMX 40\n";
+	p += "NUMY 20\n";
 	p += "BTYP 1\n";
 	p += "\n";
 	p += "# Particles\n";
-	p += "NTYP 3\n";
-	p += "NUM1 50\n";
-	p += "NUM2 20\n";
-	p += "NUM3 30\n";
+	p += "NTYP 2\n";
+	p += "NUM1 100\n";
+	p += "NUM2 100\n";
+	p += "NUM3 100\n";
 	p += "COF1 1\n";
 	p += "COF2 2\n";
 	p += "COF3 3\n";
+	p += "\n";
+	p += "# Scenario (0-4)\n";
+	p += "SCEN 0\n";
 	p += "\n";
 	
 	params = p;
@@ -102,6 +110,7 @@ function readParams() {
 		var cof = getValue("COF" + j).from(taIn);
 		Cofs.push(cof);
 	}
+	NumsL = [];
 	
 	tbeg = getValue("TBEG").from(taIn);
 	tend = getValue("TEND").from(taIn);
@@ -115,8 +124,8 @@ function readParams() {
 	t = tbeg;
 
 	XMIN = 0;
-	XMAX = caOut.width;
-	YMIN = caOut.height;
+	XMAX = caOut2.width;
+	YMIN = caOut2.height;
 	YMAX = 0;
 	
 	W = [];
@@ -143,25 +152,57 @@ function readParams() {
 		["#fff", "#fff"],
 		["#000", "#000"],
 		["#aaf", "#00f"],
-		["#afa", "#0f0"],
-		["#faa", "#f00"]
+		["#faa", "#f00"],
+		["#afa", "#0f0"]
 	];
 	
+	scen = getValue("SCEN").from(taIn);
+	
 	N = 0;
-	for(var i = 0; i < Ntyp; i++) {
-		N += Nums[i];
-		var j = 0;
-		while(j < Nums[i]) {
-			var x = Random.randInt(0, Numx - 1);
-			var y = Random.randInt(0, Numy - 1);
-			if(W[y][x] == 0) {
-				W[y][x] = i + 2;
-				j++;
+	if(scen == 0) {
+		for(var i = 0; i < Ntyp; i++) {
+			N += Nums[i];
+			var j = 0;
+			while(j < Nums[i]) {
+				var x = Random.randInt(0, Numx - 1);
+				var y = Random.randInt(0, Numy - 1);
+				if(W[y][x] == 0) {
+					W[y][x] = i + 2;
+					j++;
+				}
+			}
+		}
+	} else if(1 <= scen && scen <= 4) {
+		var n = scen + 1;
+		for(var j = 0; j < Numy / n; j++) {
+			W[n * j][Numx / 2] = 1;
+		}
+		for(var i = 0; i < 2; i++) {
+			N += Nums[i];
+			var j = 0;
+			while(j < Nums[i]) {
+				var x = Random.randInt(
+					i * Numx / 2,
+					(i + 1) * Numx / 2
+				);
+				var y = Random.randInt(0, Numy - 1);
+				if(W[y][x] == 0) {
+					W[y][x] = i + 2;
+					j++;
+				}
 			}
 		}
 	}
 	
+	clearCanvas(caOut);
 	drawMatrixOnCanvas(W);
+	clearCanvas(caOut2);
+
+	// Data
+	series = [
+		[0, Nums[0], []],
+		[0, Nums[1], []]
+	];
 }
 
 
@@ -226,10 +267,21 @@ function createVisualElements() {
 	// Create canvas for output
 	caOut = document.createElement("canvas");
 	caOut.width = "400";
-	caOut.height = "400";
+	caOut.height = "200";
 	with(caOut.style) {
 		width = caOut.width +  "px";
 		height = caOut.height +  "px";
+		border = "1px solid #aaa";
+		background = "#fff";
+	}
+	
+	// Create 2nd canvas for output
+	caOut2 = document.createElement("canvas");
+	caOut2.width = "400";
+	caOut2.height = "195";
+	with(caOut2.style) {
+		width = caOut2.width +  "px";
+		height = caOut2.height +  "px";
 		border = "1px solid #aaa";
 		background = "#fff";
 	}
@@ -264,6 +316,7 @@ function createVisualElements() {
 		dvLeft.append(btInfo);
 	document.body.append(dvRight);
 		dvRight.append(caOut);
+		dvRight.append(caOut2);
 }
 
 
@@ -328,13 +381,39 @@ function simulate() {
 	if(iter == 0) {
 		var tt = "0000" + t;
 		tt = tt.slice(-4);
-		var text = tt;
+		
+		for(var i = 0; i < 2; i++) {
+			NumsL[i] = 0;
+		}
+		
+		for(var j = 0; j < Numy; j++) {
+			for(var i = 0; i < Numx / 2; i++) {
+				if(W[j][i] > 1) {
+					NumsL[W[j][i] - 2]++;
+				}
+			}			
+		}
+		
+		series[0][2].push(NumsL[0]);
+		if(series[0][2].length > 400) series[0][2].splice(0, 1);
+		
+		series[1][2].push(NumsL[1]);
+		if(series[1][2].length > 400) series[1][2].splice(0, 1);
+		
+
+		var text = tt + " "
+		text += NumsL[0] + " ";
+		text += NumsL[1] + " ";
 		addText(text + "\n").to(taOut);
+		
+		updateMatrix(W);
+		clearCanvas(caOut);
+		drawMatrixOnCanvas(W);
+		
+		clearCanvas(caOut2);
+		drawSeriesOnCanvas(series, caOut2);
 	}
 	
-	updateMatrix(W);
-	clearCanvas(caOut);
-	drawMatrixOnCanvas(W);
 	
 	if(t >= tend) {
 		btLoad.disabled = false;
@@ -468,11 +547,67 @@ function updateMatrix() {
 				if(W[ydest][xdest] == 0) {
 					W[ydest][xdest] = W[ysrc][xsrc];
 					W[ysrc][xsrc] = 0;
-					i++;					
-					
-					addText(typ + "\n").to(taOut);
+					i++;
 				}
 			}
 		}
+	}
+}
+
+
+// Draw graph of data on canvas
+function drawSeriesOnCanvas() {
+	var series = arguments[0];
+	var Nseries = series.length;
+	
+	var can = arguments[1];
+	var width = can.width;
+	var height = can.height;
+	var cx = can.getContext("2d");
+	
+	//var h = height / Nseries;
+	var h = height;
+	var XMIN = 0;
+	var XMAX = width;
+	var YMIN = height;
+	var YMAX = 0;
+	
+	var xmin = 0;
+	var xmax = 400;
+	var ymax = 1;
+	var ymin = 0;
+	
+	var margin = 10;
+	
+	for(var s = 0; s < Nseries; s++) {
+		//YMAX = s * h + margin;
+		YMAX = margin;
+		YMIN = YMAX + h - margin;
+		ymin = series[s][0];
+		ymax = series[s][1];
+		var N = series[s][2].length;
+		cx.beginPath();
+		cx.strokeStyle = colors[s + 2][1];
+		cx.lineWidth = 2;
+		for(var i = 0; i < N; i++) {
+			var x = i;
+			var y = series[s][2][i];
+			var p = transform(x, y);
+			if(i == 0) {
+				cx.moveTo(p[0], p[1]);
+			} else {
+				cx.lineTo(p[0], p[1]);
+			}
+		}
+		cx.stroke();
+		cx.closePath();
+	}
+	
+	function transform(x, y) {
+		var XX = (x - xmin) / (xmax - xmin) * (XMAX - XMIN)
+		XX += XMIN;
+		var YY = (y - ymin) / (ymax - ymin) * (YMAX - YMIN)
+		YY += YMIN;
+		return [XX, YY];
 	}
 }
